a264330a3df8fdb57a2d7ccc91e692fed2104999
RoseReyes <roseannlorinreyes@gmail.com>
2025-02-08 21:12:58 -0800

    output dynamic values

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,3 +1,9 @@
+const reactDescriptions = ['Fundamental', 'Core', 'Crucial'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
   return (
     <header>
@@ -7,8 +13,8 @@ function Header() {
       />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {reactDescriptions[genRandomInt(2)]} React concepts you will need for
+        almost any app you are going to build!
       </p>
     </header>
   );
