27039c6204e1a7e8e2eb205c43b754c57d8c341d
RoseReyes <roseannlorinreyes@gmail.com>
2025-02-13 19:45:28 -0800

    storing components in a separate files and folders

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,41 +1,6 @@
 import { CORE_CONCEPTS } from './data';
-import reactImg from './assets/react-core-concepts.png';
-
-const reactDescriptions = ['Fundamental', 'Core', 'Crucial'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img
-        src={reactImg}
-        alt='Stylized atom'
-      />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img
-        src={image}
-        alt={title}></img>
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import CoreConcept from './components/CoreConcept.jsx';
+import Header from './components/Header.jsx';
 
 function App() {
   return (
