f885a77ebf772273cb7844ec73ab11bbdf79587c
RoseReyes <roseannlorinreyes@gmail.com>
2025-02-17 16:02:54 -0800

    passing function as a value to a prop

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -4,6 +4,10 @@ import Header from '../src/components/Header/Header.jsx';
 import TabButton from './components/TabButton/TabButton.jsx';
 
 function App() {
+  function handleSelect() {
+    console.log('Hello World');
+  }
+
   return (
     <div>
       <Header />
@@ -20,10 +24,10 @@ function App() {
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
           </menu>
         </section>
       </main>
