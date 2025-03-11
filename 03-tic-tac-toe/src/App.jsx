a33fe622497b284bd24db3576db2599f507f7627
RoseReyes <roseannlorinreyes@gmail.com>
2025-03-10 20:55:26 -0700

    alternative way to lift state up

--- 03-tic-tac-toe/src/App.jsx
+++ 03-tic-tac-toe/src/App.jsx
@@ -49,7 +49,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -91,11 +91,13 @@ function App() {
             initialName='Player 1'
             symbol='X'
             isActive={activePlayer === 'X'}
+            onChangeName={handlePlayerNameChange}
           />
           <Player
             initialName='Player 2'
             symbol='O'
             isActive={activePlayer === 'O'}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
         {(winner || hasDraw) && (
