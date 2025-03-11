cadf7b22734bb53863f461ff0b55e16a0f60715a
RoseReyes <roseannlorinreyes@gmail.com>
2025-03-10 20:48:08 -0700

    when to not update state

--- 03-tic-tac-toe/src/App.jsx
+++ 03-tic-tac-toe/src/App.jsx
@@ -21,10 +21,11 @@ function deriveActivePlayer(gameTurns) {
 }
 
 function App() {
+  const [players, setPlayers] = useState({ X: 'Player 1', O: 'Player 2' });
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map((array) => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -67,6 +68,19 @@ function App() {
     });
   }
 
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName,
+      };
+    });
+  }
+
   return (
     <main>
       <div id='game-container'>
@@ -84,7 +98,12 @@ function App() {
             isActive={activePlayer === 'O'}
           />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} />}
+        {(winner || hasDraw) && (
+          <GameOver
+            winner={winner}
+            onRestart={handleRestart}
+          />
+        )}
         <GameBoard
           onGameBoard={handleGameBoard}
           activePlayerSymbol={activePlayer}
