fede2b82c95039a2a81715c90bcd770fad13c8bc
RoseReyes <roseannlorinreyes@gmail.com>
2025-03-10 17:43:03 -0700

    lifting state up

--- 03-tic-tac-toe/src/App.jsx
+++ 03-tic-tac-toe/src/App.jsx
@@ -1,21 +1,37 @@
 import GameBoard from './components/GameBoard/GameBoard';
 import Player from './components/Player/Player';
+import { useState } from 'react';
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleGameBoard() {
+    setActivePlayer((currentActivePlayer) =>
+      currentActivePlayer === 'X' ? 'O' : 'X'
+    );
+  }
+
   return (
     <main>
       <div id='game-container'>
-        <ol id='players'>
+        <ol
+          id='players'
+          className='highlight-player'>
           <Player
             initialName='Player 1'
             symbol='X'
+            isActive={activePlayer === 'X'}
           />
           <Player
             initialName='Player 2'
             symbol='O'
+            isActive={activePlayer === 'O'}
           />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onGameBoard={handleGameBoard}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
       LOG
     </main>
--- 03-tic-tac-toe/src/components/GameBoard/GameBoard.jsx
+++ 03-tic-tac-toe/src/components/GameBoard/GameBoard.jsx
@@ -1,18 +1,36 @@
+import { useState } from 'react';
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onGameBoard, activePlayerSymbol }) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleGameBoard(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedBoard;
+    });
+
+    onGameBoard();
+  }
   return (
     <ol id='game-board'>
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleGameBoard(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
