8fa8cbed2207ede225445ab0581b9867179423b0
RoseReyes <roseannlorinreyes@gmail.com>
2025-05-12 18:11:54 -0700

    added result table component

---
+++ 04-investment-calculator/src/components/result-table/ResultTable.jsx
@@ -0,0 +1,15 @@
+export function ResultTable() {
+  return (
+    <table id='result'>
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Interest</th>
+          <th>EOF value</th>
+          <th>Annual Investment</th>
+        </tr>
+        <tbody></tbody>
+      </thead>
+    </table>
+  );
+}
--- 04-investment-calculator/src/components/user-input/UserInput.jsx
+++ 04-investment-calculator/src/components/user-input/UserInput.jsx
@@ -1,22 +1,40 @@
+import { calculateInvestmentResults, formatter } from '../../util/investment';
+
 export default function UserInput() {
+  const handleInput = (event) => {
+    console.log(event.target.value, 'event object');
+  };
+
   return (
     <>
       <div id='user-input'>
-        <div class='input-group'>
+        <div className='input-group'>
           <label id='user-label'>INITIAL INVESTMENTS</label>
           <label id='user-label'>ANNUAL INVESTMENTS</label>
         </div>
-        <div class='input-group'>
-          <input type='number' />
-          <input type='number' />
+        <div className='input-group'>
+          <input
+            type='number'
+            onChange={(e) => handleInput(e)}
+          />
+          <input
+            type='number'
+            onChange={(e) => handleInput(e)}
+          />
         </div>
-        <div class='input-group'>
+        <div className='input-group'>
           <label id='user-label'>EXPECTED RETURNS</label>
           <label id='user-label'>DURATION</label>
         </div>
-        <div class='input-group'>
-          <input type='number' />
-          <input type='number' />
+        <div className='input-group'>
+          <input
+            type='number'
+            onChange={(e) => handleInput(e)}
+          />
+          <input
+            type='number'
+            onChange={(e) => handleInput(e)}
+          />
         </div>
       </div>
     </>
