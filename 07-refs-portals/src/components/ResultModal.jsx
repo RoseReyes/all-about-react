837dbde5052fe20c246650a62100290a27247a37
RoseReyes <roseannlorinreyes@gmail.com>
2025-05-23 23:14:00 -0700

    add results component

---
+++ 07-refs-portals/src/components/ResultModal.jsx
@@ -0,0 +1,18 @@
+const ResultModal = ({ result, targetTime }) => {
+  return (
+    <dialog>
+      <h2>You {result}</h2>
+      <p>
+        The target time was <strong>{targetTime}</strong>
+      </p>
+      <p>
+        You stopped the timer with <strong>X seconds left.</strong>
+      </p>
+      <form method='dialog'>
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+};
+
+export default ResultModal;
