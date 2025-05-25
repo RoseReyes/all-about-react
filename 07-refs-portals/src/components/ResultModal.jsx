6ddc27042b9c15ccc57c05ff9ae38970e7097b80
RoseReyes <roseannlorinreyes@gmail.com>
2025-05-24 18:44:51 -0700

    expose callable function inside components

--- 07-refs-portals/src/components/ResultModal.jsx
+++ 07-refs-portals/src/components/ResultModal.jsx
@@ -1,8 +1,19 @@
+import { useImperativeHandle, useRef } from 'react';
+
 const ResultModal = ({ result, targetTime, ref }) => {
+  const dialog = useRef();
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
   return (
     <dialog
       className='result-modal'
-      ref={ref}>
+      ref={dialog}>
       <h2>You {result}</h2>
       <p>
         The target time was <strong>{targetTime}</strong>
