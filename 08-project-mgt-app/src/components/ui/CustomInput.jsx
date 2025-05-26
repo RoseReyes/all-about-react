f04b892ed297e56c1ef8eaddcd8493fb75156046
RoseReyes <roseannlorinreyes@gmail.com>
2025-05-25 18:35:55 -0700

    apply DRY code when element has the same styling

--- 08-project-mgt-app/src/components/ui/CustomInput.jsx
+++ 08-project-mgt-app/src/components/ui/CustomInput.jsx
@@ -1,4 +1,6 @@
 const Input = ({ label, isTextarea, ...props }) => {
+  const classes =
+    'w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600';
   return (
     <>
       <p>
@@ -7,12 +9,12 @@ const Input = ({ label, isTextarea, ...props }) => {
         </label>
         {isTextarea ? (
           <textarea
-            className='w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600'
+            className={classes}
             {...props}
           />
         ) : (
           <input
-            className='w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600'
+            className={classes}
             {...props}
           />
         )}
