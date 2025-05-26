9bc7aaffc9125a40cae64473b7201a2069c1a1ab
RoseReyes <roseannlorinreyes@gmail.com>
2025-05-25 18:34:09 -0700

    styling input, buttons in custom input component

--- 08-project-mgt-app/src/components/NewProject.jsx
+++ 08-project-mgt-app/src/components/NewProject.jsx
@@ -2,32 +2,34 @@ import Input from './ui/CustomInput.jsx';
 
 const NewProject = () => {
   return (
-    <>
-      <div>
-        <menu className='flex items-center justify-end gap-4 my-4'>
-          <li>
-            <button>Cancel</button>
-          </li>
-          <li>
-            <button>Save</button>
-          </li>
-        </menu>
-      </div>
+    <div className='w-[35rem] mt-16'>
+      <menu className='flex items-center justify-end gap-4 my-4'>
+        <li>
+          <button className='text-stone-800 hover:text-stone-950'>
+            Cancel
+          </button>
+        </li>
+        <li>
+          <button className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'>
+            Save
+          </button>
+        </li>
+      </menu>
       <div>
         <Input
-          label='TITLE'
+          label='Title'
           type='text'
         />
         <Input
-          label='DESCRIPTION'
+          label='Description'
           textarea
         />
         <Input
-          label='DUE DATE'
+          label='Due Date'
           type='date'
         />
       </div>
-    </>
+    </div>
   );
 };
 
