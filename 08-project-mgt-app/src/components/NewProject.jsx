92106cdf66572e0e0b1d161d497426e416dc9f59
RoseReyes <roseannlorinreyes@gmail.com>
2025-05-26 22:48:09 -0700

    add validation todo

--- 08-project-mgt-app/src/components/NewProject.jsx
+++ 08-project-mgt-app/src/components/NewProject.jsx
@@ -6,6 +6,14 @@ const NewProject = () => {
   const descriptionRef = useRef();
   const dueDateRef = useRef();
 
+  const handleSave = () => {
+    const enteredTitle = titleRef.current.value;
+    const enteredDescription = descriptionRef.current.value;
+    const enteredDueDate = dueDateRef.current.value;
+
+    // Todo: validation
+  };
+
   return (
     <div className='w-[35rem] mt-16'>
       <menu className='flex items-center justify-end gap-4 my-4'>
@@ -15,7 +23,9 @@ const NewProject = () => {
           </button>
         </li>
         <li>
-          <button className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'>
+          <button
+            className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'
+            onClick={handleSave}>
             Save
           </button>
         </li>
