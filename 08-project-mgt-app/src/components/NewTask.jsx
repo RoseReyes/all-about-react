2e523c1b3905d3b0ac0e5d944a9606cdc01ba889
RoseReyes <roseannlorinreyes@gmail.com>
2025-05-31 16:12:08 -0700

    2-way binding exercise

--- 08-project-mgt-app/src/components/NewTask.jsx
+++ 08-project-mgt-app/src/components/NewTask.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
+
 export const NewTask = () => {
+  const [enteredTask, setEnteredTask] = useState('');
+
+  const handleChange = (event) => {
+    setEnteredTask(event.target.value);
+  };
+
   return (
     <>
       <div className='flex items-center gap-4'>
         <input
+          value={enteredTask}
           type='text'
           className='w-64 px-2 py-1 rounded-sm bg-stone-200'
+          onChange={handleChange}
         />
         <button className='text-stone-700 hover:text-stone-950'>
           Add Task
