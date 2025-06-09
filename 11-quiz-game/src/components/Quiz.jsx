6224f0c0fd7739d7a9753b1f7189f8f436ed6574
RoseReyes <roseannlorinreyes@gmail.com>
2025-06-09 14:26:38 -0700

    found a bug on use state updating function without return

--- 11-quiz-game/src/components/Quiz.jsx
+++ 11-quiz-game/src/components/Quiz.jsx
@@ -5,24 +5,30 @@ const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
 
+  console.log(activeQuestionIndex, 'index');
+
   const handleSelectAnswer = (selectedAnswer) => {
     setUserAnswers((prevUserAnswers) => {
-      [...prevUserAnswers, selectedAnswer];
+      return [...prevUserAnswers, selectedAnswer];
     });
   };
 
   return (
-    <div id='question'>
-      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-      <ul id='answers'>
-        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
-          <li
-            key={answer}
-            className='answer'>
-            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-          </li>
-        ))}
-      </ul>
+    <div id='quiz'>
+      <div id='question'>
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id='answers'>
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li
+              key={answer}
+              className='answer'>
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
