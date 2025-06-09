9a8ae86e5a7592349817d3aa93c2c923d78a7c8a
RoseReyes <roseannlorinreyes@gmail.com>
2025-06-09 14:04:46 -0700

    added answers and question display

--- 11-quiz-game/src/components/Quiz.jsx
+++ 11-quiz-game/src/components/Quiz.jsx
@@ -1,10 +1,31 @@
+import QUESTIONS from '../questions.js';
 import { useState } from 'react';
 
 const Quiz = () => {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>Currently active Questions</p>;
+  const activeQuestionIndex = userAnswers.length;
+
+  const randomAnswer = QUESTIONS[activeQuestionIndex].answers.sort(
+    () => Math.random() - 0.5
+  );
+
+  const handleSelectAnswer = () => {};
+
+  return (
+    <div id='question'>
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id='answers'>
+        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          <li
+            key={answer}
+            className='answer'>
+            <button onClick={handleSelectAnswer}>{answer}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Quiz;
