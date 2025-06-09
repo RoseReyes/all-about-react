c262b4587dd84647efa05bedc9307218ace5deca
RoseReyes <roseannlorinreyes@gmail.com>
2025-06-09 12:37:31 -0700

    added state in quiz components and questions.js mock data

--- 11-quiz-game/src/components/Quiz.jsx
+++ 11-quiz-game/src/components/Quiz.jsx
@@ -1,3 +1,10 @@
-const Quiz = () => {};
+import { useState } from 'react';
+
+const Quiz = () => {
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  return <p>Currently active Questions</p>;
+};
 
 export default Quiz;
