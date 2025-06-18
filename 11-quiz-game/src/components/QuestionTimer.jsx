5ef1cf3b7d245755a139931e60bf2b88a189c4e6
RoseReyes <roseannlorinreyes@gmail.com>
2025-06-18 15:13:10 -0700

    useEffect clean-up function practice

--- 11-quiz-game/src/components/QuestionTimer.jsx
+++ 11-quiz-game/src/components/QuestionTimer.jsx
@@ -5,13 +5,23 @@ export const QuestionTimer = ({ timeOut, onTimeout }) => {
   const [remainingTime, setRemainingTime] = useState(timeOut);
 
   useEffect(() => {
-    setTimeout(onTimeout, timeOut);
+    console.log('SET TIMEOUT');
+    const timer = setTimeout(onTimeout, timeOut);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, [timeOut, onTimeout]);
 
   useEffect(() => {
-    setInterval(() => {
+    console.log('SET INTERVAL');
+    const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   return (
