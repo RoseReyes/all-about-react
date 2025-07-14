567809750d9993026b7cd3f9c55c0ba4492cfe39
RoseReyes <roseannlorinreyes@gmail.com>
2025-07-14 16:01:47 -0700

    handle page error

--- 14-data-fetching/src/components/AvailablePlaces.jsx
+++ 14-data-fetching/src/components/AvailablePlaces.jsx
@@ -1,22 +1,46 @@
 import { useEffect, useState } from 'react';
 
+import Error from './Error.jsx';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch('http://localhost:3000/places');
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+
+      try {
+        const response = await fetch('http://localhost:3000/placees');
+        const resData = await response.json();
+        setAvailablePlaces(resData.places);
+
+        if (!response.ok) {
+          throw new Error('Failed  to fetch places');
+        }
+      } catch (error) {
+        setError({
+          message:
+            error.message || 'Could not fetch places please try again later.',
+        });
+      }
+
       setIsFetching(false);
     }
 
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return (
+      <Error
+        title='An error occurred!'
+        message={error.message}
+      />
+    );
+  }
 
   return (
     <Places
