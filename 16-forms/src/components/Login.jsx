58786b33cc82a1680aee33c6eceb79fa3671c803
RoseReyes <roseannlorinreyes@gmail.com>
2025-08-07 22:37:21 -0700

    forms

--- 16-forms/src/components/Login.jsx
+++ 16-forms/src/components/Login.jsx
@@ -3,22 +3,31 @@ export default function Login() {
     <form>
       <h2>Login</h2>
 
-      <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+      <div className='control-row'>
+        <div className='control no-margin'>
+          <label htmlFor='email'>Email</label>
+          <input
+            id='email'
+            type='email'
+            name='email'
+          />
         </div>
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+        <div className='control no-margin'>
+          <label htmlFor='password'>Password</label>
+          <input
+            id='password'
+            type='password'
+            name='password'
+          />
         </div>
       </div>
 
-      <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+      <p className='form-actions'>
+        <button className='button button-flat'>Reset</button>
+        <button className='button'>Login</button>
       </p>
+      <div></div>
     </form>
   );
 }
