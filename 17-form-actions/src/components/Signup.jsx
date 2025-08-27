62df9a10f9b83001f7af7adb0d5085c919bbb564
RoseReyes <roseannlorinreyes@gmail.com>
2025-08-26 20:34:09 -0700

    form actions with one  field on handle submit

--- 17-form-actions/src/components/Signup.jsx
+++ 17-form-actions/src/components/Signup.jsx
@@ -1,11 +1,11 @@
 export default function Signup() {
-  const handleSubmit = (formData) => {
+  const signupAction = (formData) => {
     const enteredEmail = formData.get('email');
     console.log(enteredEmail);
   };
 
   return (
-    <form action={handleSubmit}>
+    <form action={signupAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
