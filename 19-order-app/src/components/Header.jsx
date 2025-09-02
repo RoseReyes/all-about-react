5363a0725efeb720e91048a4ae5cb0f1052bce24
RoseReyes <roseannlorinreyes@gmail.com>
2025-09-02 15:00:42 -0700

    header components

---
+++ 19-order-app/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import foodAppImg from '../assets/logo.jpg';
+
+const Header = () => {
+  return (
+    <header id='main-header'>
+      <div id='title'>
+        <img
+          src={foodAppImg}
+          alt='Food App icon'></img>
+        <h1>REACTFOOD</h1>
+      </div>
+      <nav>
+        <button className='cart'>Cart (0)</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
