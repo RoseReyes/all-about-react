1072e88209a94c153210d0d9210d3703b93b5125
RoseReyes <roseannlorinreyes@gmail.com>
2025-09-07 13:19:29 -0700

    more cart context

--- 19-order-app/src/App.jsx
+++ 19-order-app/src/App.jsx
@@ -1,12 +1,13 @@
+import CartContextProvider from './store/food-cart-context.jsx';
 import { Header } from './components/Header';
 import { Meals } from './components/Meals';
 
 function App() {
   return (
-    <>
+    <CartContextProvider>
       <Header />
       <Meals />
-    </>
+    </CartContextProvider>
   );
 }
 
--- 19-order-app/src/store/food-cart-context.jsx
+++ 19-order-app/src/store/food-cart-context.jsx
@@ -1,7 +1,31 @@
-import { createContext, useContext, useReducer } from 'react';
+import { createContext, useReducer } from 'react';
 
 export const CartContext = createContext({
+  items: [],
   addItemToCart: () => {},
 });
 
-const addItemToCart = () => {};
+export const CartContextProvider = ({ children }) => {
+  const [shoppingCartState, shoppingCartDispatch] = useReducer(
+    shoppingCartReducer,
+    {
+      items: [],
+    }
+  );
+
+  const handleItemToCart = (id) => {
+    shoppingCartDispatch({
+      type: 'ADD_ITEM',
+      payload: id,
+    });
+  };
+
+  const ctxValue = {
+    items: shoppingCartState.items,
+    addItemToCart: handleItemToCart,
+  };
+
+  return (
+    <CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>
+  );
+};
