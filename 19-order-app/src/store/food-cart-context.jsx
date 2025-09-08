afc88716aa3ca9bfebecd691aec6912222a97de3
RoseReyes <roseannlorinreyes@gmail.com>
2025-09-08 12:27:36 -0700

    more cart context

--- 19-order-app/src/store/food-cart-context.jsx
+++ 19-order-app/src/store/food-cart-context.jsx
@@ -4,22 +4,45 @@ export const CartContext = createContext({
   items: [],
   addItemToCart: () => {},
   removeItemToCart: () => {},
+  updateItemQuantity: () => {},
 });
 
-export const FoodCartContextProvider = ({ children }) => {
-  const [shoppingCartState, shoppingCartDispatch] = useReducer(
-    shoppingCartReducer,
-    {
-      items: [],
-    }
-  );
+// return updated state
+const foodCartReducer = (state, action) => {
+  const updatedItems = [...state.items];
 
-  const handleItemToCart = (id) => {
-    shoppingCartDispatch({
-      type: 'ADD_ITEM',
-      payload: id,
-    });
-  };
+  switch (action.type) {
+    case 'ADD_ITEM':
+      const existingCartItemIndex = updatedItems.findIndex(
+        (cartItem) => cartItem.id === action.item.id
+      );
+      const existingCartItem = updatedItems[existingCartItemIndex];
+
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity + 1,
+        };
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        updatedItems.push({
+          ...action.item,
+          quantity: 1,
+        });
+      }
+
+      return {
+        ...state,
+      };
+    case 'REMOVE_ITEM':
+  }
+};
+export const FoodCartContextProvider = ({ children }) => {
+  const [cartState, cartDispatch] = useReducer(foodCartReducer, {
+    items: [],
+  });
+
+  const handleItemToCart = (id) => {};
 
   const ctxValue = {
     items: shoppingCartState.items,
