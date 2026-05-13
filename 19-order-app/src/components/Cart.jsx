ce843f55bd842e91c75905432e7127a06bf10b20
RoseReyes <roseannlorinreyes@gmail.com>
2026-05-12 20:25:07 -0700

    Cart practice

--- 19-order-app/src/components/Cart.jsx
+++ 19-order-app/src/components/Cart.jsx
@@ -1,1 +1,21 @@
+import CartContext from '../store/cart-context.js';
 import Modal from './UI/Modal.jsx';
+import { useContext } from 'react';
+
+const Cart = () => {
+  const cartCtx = useContext(CartContext);
+  return (
+    <Modal className='cart'>
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.amount}
+          </li>
+        ))}
+      </ul>
+    </Modal>
+  );
+};
+
+export default Cart;
